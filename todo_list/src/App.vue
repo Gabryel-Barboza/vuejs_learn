485cef24c3ca9019af84ec698d6d6ff096e90061
Gabryel_Brz <gabrielbarbosa.alternativa@gmail.com>
2025-05-20 20:45:34 -0300

    Refactor

--- todo_list/src/App.vue
+++ todo_list/src/App.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { RouterLink, RouterView } from 'vue-router';
+import HelloWorld from './components/HelloWorld.vue';
 </script>
 
 <template>
+  <!--HMR = Hot Module Replacement, adiciona alterações sem perder o estado da app-->
   <header>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
--- todo_list/vite.config.ts
+++ todo_list/vite.config.ts
@@ -1,18 +1,25 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vueDevTools from 'vite-plugin-vue-devtools';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  // Extensões para o Vite, adicionando tailwindcss
+  plugins: [vue(), vueDevTools(), tailwindcss()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      // Objeto import para recuperar variáveis de ambiente em .env, disponível pelo Vite
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-})
+  // Alterando configurações do app
+  server: {
+    port: 3000,
+  },
+  build: {
+    outDir: 'build',
+  },
+});
